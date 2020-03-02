8cc86ab3a801a67a8533437f9abed750a01fe02b
atiqueahmedziad <softfilebd@gmail.com>
2020-03-02 21:58:07 +0600

    Priority Scheduling (Non Preemptive) code updated

--- Process_Scheduler/Priority_Scheduling_Non_preemptive.cs
+++ Process_Scheduler/Priority_Scheduling_Non_preemptive.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 /* 
 
-Based on Non Preemptive Algorithm
+Based on Non Preemtive Algorithm
 
 */
 
@@ -22,36 +22,97 @@ namespace PS
         public Process(double processId, double arrivalTime, double cpuTime, double priority)
         {
             this.arrivalTime = arrivalTime;
-            this.processId = processId;
             this.cpuTime = cpuTime;
+            this.processId = processId;
             this.priority = priority;
         }
-    }
+    };
 
     class MainClass
     {
+
+        public static List<Process> sortedList(List<Process> processes)
+        {
+            double sumOfCpuTime = 0;
+            int itemCount = 0;
+
+            // Since we are already sending 1st process in the sortedProcess List
+            int itemInQueue = processes.Count - 1;
+
+            // Sort the processes in assending order according to their arrival time
+            processes = processes.OrderBy(process => process.arrivalTime).ToList();
+
+            List<Process> sortedProcess = new List<Process>();
+
+            sortedProcess.Add(processes[0]);
+
+            for (int i = 1; i < processes.Count; i++)
+            {
+                List<Process> temp = new List<Process>();
+
+                sumOfCpuTime += processes[i - 1].cpuTime;
+
+                for (int j = itemCount + 1; j < processes.Count; j++)
+                {
+                    if (processes[j].arrivalTime <= sumOfCpuTime)
+                    {
+                        temp.Add(processes[j]);
+                        itemCount++;
+                    }
+                }
+                if (temp.Count != 0 & itemCount != itemInQueue)
+                {
+                    temp = temp.OrderBy(process => process.priority).ToList();
+                    //sortedProcess.AddRange(temp);
+                    sortedProcess.Add(temp[0]);
+                    itemCount = itemCount - temp.Count + 1;
+                }
+
+                else if (temp.Count != 0 & itemCount == itemInQueue)
+                {
+                    temp = temp.OrderBy(process => process.priority).ToList();
+                    sortedProcess.AddRange(temp);
+                }
+
+                if (itemCount == itemInQueue)
+                {
+                    break;
+                }
+            }
+
+            return sortedProcess;
+        }
+
         public static void Main(string[] args)
         {
-            List<Process> processes = new List<Process>();
-
-            Console.WriteLine("How many process do you want to enter ?");
+            Console.WriteLine($"How many process do you want to enter: ");
             int n = Convert.ToInt32(Console.ReadLine());
+
+            List<Process> processes = new List<Process>();
 
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine($"Enter CPU time for proceess {i + 1}: ");
+                Console.WriteLine($"Enter arrival time for process {i + 1}: ");
+                double arrivalTime = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"Enter cpu time for process {i + 1}: ");
                 double cpuTime = Convert.ToDouble(Console.ReadLine());
 
-                Console.WriteLine($"Enter priority for proceess {i + 1}: ");
+                Console.WriteLine($"Enter priority for process {i + 1}: ");
                 double priority = Convert.ToDouble(Console.ReadLine());
 
-                // Arrival time is assigned 0 for all process
-                Process process = new Process(i + 1, 0, cpuTime, priority);
+                Process process = new Process(i + 1, arrivalTime, cpuTime, priority);
 
                 processes.Add(process);
             }
 
-            processes = processes.OrderBy(process => process.priority).ToList();
+            processes = sortedList(processes);
+
+            // If you want to see the gantt chart, uncomment this
+            //for (int i = 0; i < processes.Count; i++)
+            //{
+            //    Console.WriteLine($"Process {processes[i].processId}");
+            //}
 
             double totalWaitTime = 0, totalTurnAroundTime = 0;
 
